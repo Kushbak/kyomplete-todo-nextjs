c3670bb8ebc281c7e48d06badf3c88dedc3f5c8c
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-10-29 03:11:45 +0600

    add types file

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export interface ITask {
+  id: number
+  title: string
+  is_completed: boolean
+  due_date?: string
+  due_time?: string
+
+  assigned_to?: any
+  author?: any 
+}
+
+export type ITaskCreate = Omit<ITask, 'id' | 'is_completed'>
+
+export interface IModal {
+  title: string
+  children: React.ReactNode
+  props: any
+}
