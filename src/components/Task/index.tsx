f9cf321e0c1978f7fb987f6df4da709f1595bcc4
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-10-30 15:11:02 +0600

    add assignee badge on task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,5 +1,5 @@
 import { ITask } from "@/types"
-import { Box, Checkbox } from "@mui/material"
+import { Box, Checkbox, Tooltip } from "@mui/material"
 import styles from './index.module.scss'
 import dayjs from '@/utils/dayjs'
 import classNames from "classnames"
@@ -15,12 +15,17 @@ const Task = ({ task }: Props) => {
   const isOverdue = (date: string) => {
     return !task.is_completed && dayjs().isAfter(date);
   }
-  const { title, due_date } = task
+  const { title, due_date, assigned_to } = task
   return (
     <Box className={classNames(styles.task, task.is_completed && styles.task_completed)}>
       <CompleteTask taskId={task.id} is_completed={task.is_completed} />
       <p className={styles.task__title}>{title}</p>
       <div className={styles.task__actions}>
+        {assigned_to?.username &&
+          <Tooltip title={assigned_to?.username}>
+            <div className={styles.task__assignee}>{assigned_to?.username[0]}</div>
+          </Tooltip>
+        }
         <ActionsPanel taskId={task.id} due_date={due_date} />
       </div>
       <div className={classNames(styles.task__due, isOverdue(due_date) && styles.task__due_over)}>
