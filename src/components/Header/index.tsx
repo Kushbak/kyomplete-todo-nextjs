ffa0aad67620847e3e6f106c2bba627680ad4b23
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-11-01 05:43:49 +0600

    add create random user to test on deploy and fix mui component errores

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,13 +2,15 @@ import { PAGES } from "@/utils/const"
 import Link from "next/link"
 import styles from './index.module.scss'
 import AddTaskButton from "../AddTaskButton"
+import CreateUserBtn from "../CreateUserBtn"
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <Link href={PAGES.HOME}>Kyomplete Todo</Link>
 
-      <div className={styles.header__addTaskBtn}>
+      <div className={styles.header__btns}>
+        <CreateUserBtn />
         <AddTaskButton />
       </div>
     </header>
